522872e1ba50304ac136437a7f7fa3d2b357e5b4
Alex Layton <alex@layton.in>
2017-03-08 14:31:42 -0500

    Don't include certain unencoded characters in path
    Specifically ignores {, }, |, \, ^, and `.

--- url-regex.js
+++ url-regex.js
@@ -9,7 +9,7 @@ module.exports = function () {
   const domain = '(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*';
   const tld = '(?:\\.(?:[a-z\\u00a1-\\uffff]{2,}))';
   const port = '(?::\\d{2,5})?';
-  const path = '(?:[/?#][^\\s"\']*)?';
+  const path = '(?:[/?#][^\\s"\'{}|\\\\^`]*)?';
   const regex = [
     '(?:' + protocol + '|www\\.)' + auth, '(?:localhost|' + ip + '|' + host + domain + tld + ')',
     port, path
